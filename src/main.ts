97465aae6828ea7eee02df2666726de34b8d7161
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-07-16 04:22:20 +0300

    install pakege

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module'; // If you have routing
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule) // Add other modules here if needed
+  ]
+}).catch(err => console.error(err));
