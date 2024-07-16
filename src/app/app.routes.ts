7b9c3b5c05aed982469900451028472855114961
Ashraf Gad <158779399+AshrafGad1001@users.noreply.github.com>
2024-07-17 00:24:43 +0300

    Fix Error and carousel-items

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Default route for unmatched paths
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  BannerImages = [
+    {
+      id: 1,
+      img: 'assets/images/banner/1.jpg',
+    },
+    {
+      id: 2,
+      img: 'assets/images/banner/2.jpg',
+    },
+    {
+      id: 3,
+      img: 'assets/images/banner/3.jpg',
+    },
+  ];
 }
